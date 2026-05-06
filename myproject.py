913718b96b59c9ac5c0ccbf9f3cf2316c61ee6bf
derrick <Derrickamza@gmail.com>
2026-05-06 08:42:17 -0700

    prepare for deployment

--- myproject.py
+++ myproject.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return" Hello world this is my first application"
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000)
+if __name__ == "__main__":
+    app.run()
